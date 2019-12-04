6645cc4c1313389c09242823cb4f7f7fc81241e4
matan hager <m4t4ngm@gmail.com>
2019-12-04 12:08:12 +0200

    Using Top Alignment

--- ManipulatedBBox/Thumbs/ResizeThumb.cs
+++ ManipulatedBBox/Thumbs/ResizeThumb.cs
@@ -50,7 +50,7 @@ namespace ManipulatedBBox.Thumbs
                 {
                     this.adorner = new Adorners.SizeAdorner(this.designerItem);
                     adornerLayer.Add(this.adorner);
-                }  
+                }
             }
         }
 
@@ -59,7 +59,7 @@ namespace ManipulatedBBox.Thumbs
             if (this.designerItem != null)
             {
                 double deltaVertical, deltaHorizontal;
-
+                // CURRENTLY vertial Alignment of cocntrol set to bottom doesnt work
                 switch (VerticalAlignment)
                 {
                     case System.Windows.VerticalAlignment.Bottom:
@@ -75,14 +75,13 @@ namespace ManipulatedBBox.Thumbs
                                 switch (this.designerItem.VerticalAlignment.ToString())
                                 {
                                     case "Top":
-                                        //MessageBox.Show("t");
                                         this.designerItem.Margin = new Thickness(current_margin.Left - deltaVertical * this.transformOrigin.Y * Math.Sin(-this.angle),
                                             current_margin.Top + (this.transformOrigin.Y * deltaVertical * (1 - Math.Cos(-this.angle))),
                                             current_margin.Right,
                                             current_margin.Bottom);
                                         break;
                                     case "Bottom":
-                                        //MessageBox.Show("b");
+                                        
                                         this.designerItem.Margin = new Thickness(current_margin.Left - deltaVertical * this.transformOrigin.Y * Math.Sin(-this.angle),
                                             current_margin.Top,
                                             current_margin.Right,
@@ -91,7 +90,7 @@ namespace ManipulatedBBox.Thumbs
                                 }
                                 break;
                         }
-                        
+
                         this.designerItem.Height -= deltaVertical;
                         break;
                     case System.Windows.VerticalAlignment.Top:
@@ -121,7 +120,7 @@ namespace ManipulatedBBox.Thumbs
                                 }
                                 break;
                         }
-                        
+
                         this.designerItem.Height -= deltaVertical;
                         break;
                     default:
@@ -153,12 +152,12 @@ namespace ManipulatedBBox.Thumbs
                                         this.designerItem.Margin = new Thickness(current_margin.Left + deltaHorizontal * Math.Cos(this.angle) + (this.transformOrigin.X * deltaHorizontal * (1 - Math.Cos(this.angle))),
                                            current_margin.Top,
                                            current_margin.Right,
-                                           current_margin.Bottom - deltaHorizontal * Math.Sin(this.angle) - this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle));
+                                           current_margin.Bottom - (deltaHorizontal * Math.Sin(this.angle) - this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle)));
                                         break;
                                 }
                                 break;
                         }
-                        
+
                         this.designerItem.Width -= deltaHorizontal;
                         break;
                     case System.Windows.HorizontalAlignment.Right:
@@ -181,14 +180,14 @@ namespace ManipulatedBBox.Thumbs
                                         break;
                                     case "Bottom":
                                         this.designerItem.Margin = new Thickness(current_margin.Left + (deltaHorizontal * this.transformOrigin.X * (1 - Math.Cos(this.angle))),
-                                            current_margin.Top ,
+                                            current_margin.Top,
                                             current_margin.Right,
                                             current_margin.Bottom + this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle));
                                         break;
                                 }
                                 break;
                         }
-                        
+
                         this.designerItem.Width -= deltaHorizontal;
                         break;
                     default:
@@ -213,4 +212,210 @@ namespace ManipulatedBBox.Thumbs
             }
         }
     }
+    //public class ResizeThumb : Thumb
+    //{
+    //    private double angle;
+    //    private Adorner adorner;
+    //    private Point transformOrigin;
+    //    private ManipulatedContentControl designerItem;
+    //    //private Canvas canvas;
+    //    private Visual parent;
+
+    //    public ResizeThumb()
+    //    {
+    //        DragStarted += new DragStartedEventHandler(this.ResizeThumb_DragStarted);
+    //        DragDelta += new DragDeltaEventHandler(this.ResizeThumb_DragDelta);
+    //        DragCompleted += new DragCompletedEventHandler(this.ResizeThumb_DragCompleted);
+    //    }
+
+    //    private void ResizeThumb_DragStarted(object sender, DragStartedEventArgs e)
+    //    {
+    //        this.designerItem = this.DataContext as ManipulatedContentControl;
+
+    //        if (this.designerItem != null)
+    //        {
+    //            this.parent = VisualTreeHelper.GetParent(this.designerItem) as Visual;
+    //            //this.canvas = VisualTreeHelper.GetParent(this.designerItem) as Canvas;
+
+    //            this.transformOrigin = this.designerItem.RenderTransformOrigin;
+
+    //            if (this.designerItem.rotateTransform != null)
+    //            {
+    //                this.angle = this.designerItem.rotateTransform.Angle * Math.PI / 180.0;
+    //            }
+    //            else
+    //            {
+    //                this.angle = 0.0d;
+    //            }
+
+    //            AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this.parent);
+    //            if (adornerLayer != null)
+    //            {
+    //                this.adorner = new Adorners.SizeAdorner(this.designerItem);
+    //                adornerLayer.Add(this.adorner);
+    //            }  
+    //        }
+    //    }
+
+    //    private void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
+    //    {
+    //        if (this.designerItem != null)
+    //        {
+    //            double deltaVertical, deltaHorizontal;
+
+    //            switch (VerticalAlignment)
+    //            {
+    //                case System.Windows.VerticalAlignment.Bottom:
+    //                    deltaVertical = Math.Min(-e.VerticalChange, this.designerItem.ActualHeight - this.designerItem.MinHeight);
+    //                    switch (this.designerItem.Parent.GetType().Name.Trim())
+    //                    {
+    //                        case "Canvas":
+    //                            Canvas.SetTop(this.designerItem, Canvas.GetTop(this.designerItem) + (this.transformOrigin.Y * deltaVertical * (1 - Math.Cos(-this.angle))));
+    //                            Canvas.SetLeft(this.designerItem, Canvas.GetLeft(this.designerItem) - deltaVertical * this.transformOrigin.Y * Math.Sin(-this.angle));
+    //                            break;
+    //                        case "Grid":
+    //                            var current_margin = this.designerItem.Margin;
+    //                            switch (this.designerItem.VerticalAlignment.ToString())
+    //                            {
+    //                                case "Top":
+    //                                    //MessageBox.Show("t");
+    //                                    this.designerItem.Margin = new Thickness(
+    //                                        current_margin.Left - deltaVertical * this.transformOrigin.Y * Math.Sin(-this.angle),
+    //                                        current_margin.Top + (this.transformOrigin.Y * deltaVertical * (1 - Math.Cos(-this.angle))),
+    //                                        current_margin.Right,
+    //                                        current_margin.Bottom);
+    //                                    break;
+    //                                case "Bottom":
+    //                                    //MessageBox.Show("b");
+    //                                    this.designerItem.Margin = new Thickness(
+    //                                        current_margin.Left - deltaVertical * this.transformOrigin.Y * Math.Sin(-this.angle),
+    //                                        current_margin.Top,
+    //                                        current_margin.Right,
+    //                                        current_margin.Bottom  (this.transformOrigin.Y * deltaVertical * (1 - Math.Cos(-this.angle))));
+    //                                    break;
+    //                            }
+    //                            break;
+    //                    }
+
+    //                    this.designerItem.Height -= deltaVertical;
+    //                    break;
+    //                case System.Windows.VerticalAlignment.Top:
+    //                    deltaVertical = Math.Min(e.VerticalChange, this.designerItem.ActualHeight - this.designerItem.MinHeight);
+    //                    switch (this.designerItem.Parent.GetType().Name.Trim())
+    //                    {
+    //                        case "Canvas":
+    //                            Canvas.SetTop(this.designerItem, Canvas.GetTop(this.designerItem) + deltaVertical * Math.Cos(-this.angle) + (this.transformOrigin.Y * deltaVertical * (1 - Math.Cos(-this.angle))));
+    //                            Canvas.SetLeft(this.designerItem, Canvas.GetLeft(this.designerItem) + deltaVertical * Math.Sin(-this.angle) - (this.transformOrigin.Y * deltaVertical * Math.Sin(-this.angle)));
+    //                            break;
+    //                        case "Grid":
+    //                            var current_margin = this.designerItem.Margin;
+    //                            switch (this.designerItem.VerticalAlignment.ToString())
+    //                            {
+    //                                case "Top":
+    //                                    this.designerItem.Margin = new Thickness(current_margin.Left + deltaVertical * Math.Sin(-this.angle) - (this.transformOrigin.Y * deltaVertical * Math.Sin(-this.angle)),
+    //                                        current_margin.Top + deltaVertical * Math.Cos(-this.angle) + (this.transformOrigin.Y * deltaVertical * (1 - Math.Cos(-this.angle))),
+    //                                        current_margin.Right,
+    //                                        current_margin.Bottom);
+    //                                    break;
+    //                                case "Bottom":
+    //                                    this.designerItem.Margin = new Thickness(current_margin.Left + deltaVertical * Math.Sin(-this.angle) - (this.transformOrigin.Y * deltaVertical * Math.Sin(-this.angle)),
+    //                                        current_margin.Top,
+    //                                        current_margin.Right,
+    //                                        current_margin.Bottom - deltaVertical * Math.Cos(-this.angle) + (this.transformOrigin.Y * deltaVertical * (1 - Math.Cos(-this.angle))));
+    //                                    break;
+    //                            }
+    //                            break;
+    //                    }
+
+    //                    this.designerItem.Height -= deltaVertical;
+    //                    break;
+    //                default:
+    //                    break;
+    //            }
+
+    //            switch (HorizontalAlignment)
+    //            {
+    //                case System.Windows.HorizontalAlignment.Left:
+    //                    deltaHorizontal = Math.Min(e.HorizontalChange, this.designerItem.ActualWidth - this.designerItem.MinWidth);
+
+    //                    switch (this.designerItem.Parent.GetType().Name.Trim())
+    //                    {
+    //                        case "Canvas":
+    //                            Canvas.SetTop(this.designerItem, Canvas.GetTop(this.designerItem) + deltaHorizontal * Math.Sin(this.angle) - this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle));
+    //                            Canvas.SetLeft(this.designerItem, Canvas.GetLeft(this.designerItem) + deltaHorizontal * Math.Cos(this.angle) + (this.transformOrigin.X * deltaHorizontal * (1 - Math.Cos(this.angle))));
+    //                            break;
+    //                        case "Grid":
+    //                            var current_margin = this.designerItem.Margin;
+    //                            switch (this.designerItem.VerticalAlignment.ToString())
+    //                            {
+    //                                case "Top":
+    //                                    this.designerItem.Margin = new Thickness(current_margin.Left + deltaHorizontal * Math.Cos(this.angle) + (this.transformOrigin.X * deltaHorizontal * (1 - Math.Cos(this.angle))),
+    //                                        current_margin.Top + deltaHorizontal * Math.Sin(this.angle) - this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle),
+    //                                        current_margin.Right,
+    //                                        current_margin.Bottom);
+    //                                    break;
+    //                                case "Bottom":
+    //                                    this.designerItem.Margin = new Thickness(current_margin.Left + deltaHorizontal * Math.Cos(this.angle) + (this.transformOrigin.X * deltaHorizontal * (1 - Math.Cos(this.angle))),
+    //                                       current_margin.Top,
+    //                                       current_margin.Right,
+    //                                       current_margin.Bottom - deltaHorizontal * Math.Sin(this.angle) - this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle));
+    //                                    break;
+    //                            }
+    //                            break;
+    //                    }
+
+    //                    this.designerItem.Width -= deltaHorizontal;
+    //                    break;
+    //                case System.Windows.HorizontalAlignment.Right:
+    //                    deltaHorizontal = Math.Min(-e.HorizontalChange, this.designerItem.ActualWidth - this.designerItem.MinWidth);
+    //                    switch (this.designerItem.Parent.GetType().Name.Trim())
+    //                    {
+    //                        case "Canvas":
+    //                            Canvas.SetTop(this.designerItem, Canvas.GetTop(this.designerItem) - this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle));
+    //                            Canvas.SetLeft(this.designerItem, Canvas.GetLeft(this.designerItem) + (deltaHorizontal * this.transformOrigin.X * (1 - Math.Cos(this.angle))));
+    //                            break;
+    //                        case "Grid":
+    //                            var current_margin = this.designerItem.Margin;
+    //                            switch (this.designerItem.VerticalAlignment.ToString())
+    //                            {
+    //                                case "Top":
+    //                                    this.designerItem.Margin = new Thickness(current_margin.Left + (deltaHorizontal * this.transformOrigin.X * (1 - Math.Cos(this.angle))),
+    //                                        current_margin.Top - this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle),
+    //                                        current_margin.Right,
+    //                                        current_margin.Bottom);
+    //                                    break;
+    //                                case "Bottom":
+    //                                    this.designerItem.Margin = new Thickness(current_margin.Left + (deltaHorizontal * this.transformOrigin.X * (1 - Math.Cos(this.angle))),
+    //                                        current_margin.Top - this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle),
+    //                                        current_margin.Right,
+    //                                        current_margin.Bottom + this.transformOrigin.X * deltaHorizontal * Math.Sin(this.angle));
+    //                                    break;
+    //                            }
+    //                            break;
+    //                    }
+
+    //                    this.designerItem.Width -= deltaHorizontal;
+    //                    break;
+    //                default:
+    //                    break;
+    //            }
+    //        }
+
+    //        e.Handled = true;
+    //    }
+
+    //    private void ResizeThumb_DragCompleted(object sender, DragCompletedEventArgs e)
+    //    {
+    //        if (this.adorner != null)
+    //        {
+    //            AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this.parent);
+    //            if (adornerLayer != null)
+    //            {
+    //                adornerLayer.Remove(this.adorner);
+    //            }
+
+    //            this.adorner = null;
+    //        }
+    //    }
+    //}
 }
